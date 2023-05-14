974a9a0b5ae40dc7813bb4051bd1bad7b4df07a3
Xavier Hernandez <xhernan.martinez@gmail.com>
2023-05-14 19:23:47 +0200

    test: tailwindcss config

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
+    <h1 class="flex justify-center mt-10 test">Test</h1>
     <NuxtWelcome />
   </div>
 </template>
+
+<style lang="postcss">
+  .test {
+    @apply bg-red-500 text-2xl;
+  }
+</style>
