bf60a1c01e2fe4dc7eedad649e48718ef966c880
Xavier Hernandez <xhernan.martinez@gmail.com>
2023-05-14 19:33:26 +0200

    chores: add css reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  //@ts-ignore[[
+  //@ts-ignore
   css: ["@/assets/css/main.css"],
   postcss: {
     plugins: {
