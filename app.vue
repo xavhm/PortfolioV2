4a6bc83dbef707795232a0076dce7b9b6d563134
Xavier Hernandez <xhernan.martinez@gmail.com>
2024-06-02 11:32:25 +0200

    chores: delete font config & svg test component

--- app.vue
+++ app.vue
@@ -1,13 +1,11 @@
 <template>
   <div>
     <h1 class="flex justify-center test">Test</h1>
-    <SVGSignature />
-    <NuxtWelcome />
   </div>
 </template>
 
 <script setup lang="ts">
-import SVGSignature from './components/SVGSignature.vue';
+
 </script>
 
 <style lang="postcss" scoped>
