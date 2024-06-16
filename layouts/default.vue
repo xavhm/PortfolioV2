0a389022dcab9da61414871c02c08c73cabbb0e9
Xavier Hernandez <xhernan.martinez@gmail.com>
2024-06-16 23:23:18 +0200

    style: reduce body max-w

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full grid grid-rows-[min-content_auto_min-content] min-h-[100dvh] mx-auto max-w-4xl px-2">
+  <div class="w-full grid grid-rows-[min-content_auto_min-content] min-h-[100dvh] mx-auto max-w-3xl px-2">
     <TheHeader />
     <slot />
     <TheFooter />
