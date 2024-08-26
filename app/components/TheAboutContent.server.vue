365d8c85b9e5f27767c07c87a1cb3846986a2bf5
Xavier Hernandez <xhernan.martinez@gmail.com>
2024-08-26 22:23:55 +0200

    style: refacto css on about page

--- app/components/TheAboutContent.server.vue
+++ app/components/TheAboutContent.server.vue
@@ -4,55 +4,69 @@
     aria-label="projects gallery"
   >
     <p
-      class="font-semibold mb-12"
+      class="title"
       title="Live long and prosper"
     >
       <span class="text-xl">🖖</span> A bit about me...
     </p>
-    <h3 class="font-semibold mb-2">
+    <h3 class="title">
       What are your aiming for in this new career?
     </h3>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       I aim to create seamless and engaging user experiences that bridge the gap between design and functionality, ultimately enhancing the way users interact with digital products.
     </p>
-    <p class="text-sm leading-6 mb-6">
+    <p class="last_paragraph">
       On the technical side, I aspire to master fullstack development, delving into server-side technologies like Go or Rust, and exploring web innovations such as WebAssembly.
     </p>
 
-    <h3 class="font-semibold mb-2">
+    <h3 class="title">
       What was your previous career in LiveOps all about?
     </h3>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       A LiveOps Manager is responsible for planning, creating, and executing in-game events, promotions, and content updates to drive player engagement and revenue for an online game.
     </p>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       The key responsibilities of the job include event planning and execution, analytics and optimization of performance metrics, and live/online operations strategy. It is a process executed through cross-functional collaboration with engineering, marketing, and customer support teams.
     </p>
-    <p class="text-sm leading-6 mb-6">
+    <p class="last_paragraph">
       The role blends analytical skills, creativity, project management abilities, and a deep understanding of games and player behavior.
     </p>
 
-    <h3 class="font-semibold mb-2">
+    <h3 class="title">
       How does it relate to frontend development?
     </h3>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       LiveOps management and frontend web development share some transferable skills and overlapping areas that connect the two roles. Both have a strong user-centered focus to enhance the user experience and drive engagement and retention, as well as create intuitive and delightful user interfaces.
     </p>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       In both cases, it should be a data-driven decision-making process based on A/B testing and metrics to iterate on UI/UX and user engagement.
     </p>
-    <p class="text-sm leading-6 mb-6">
+    <p class="last_paragraph">
       Finally, both operate in an agile and cross-functional environment with product and marketing teams.
     </p>
 
-    <h3 class="font-semibold mb-2">
+    <h3 class="title">
       What are your interests besides web development?
     </h3>
-    <p class="text-sm leading-6 mb-2">
+    <p class="paragraph">
       I am also passionate about economics, investing, and swing trading. I enjoy technical analysis and have a particular interest in cryptocurrencies—I’m a Bitcoin enthusiast! 😁.
     </p>
-    <p class="text-sm leading-6 mb-6">
+    <p class="last_paragraph">
       More broadly, I have a strong interest in technology and sciences, likes Web3, IA, physics and philosophy.
     </p>
   </section>
 </template>
+
+<style lang="postcss scoped">
+.title {
+  @apply font-semibold mb-2;
+}
+
+.paragraph {
+ @apply text-sm leading-6 mb-2;
+}
+
+.last_paragraph {
+  @apply text-sm leading-6 mb-6;
+}
+</style>
