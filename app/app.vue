f3b63347eed18843ee4d388dceff107c346b20ef
Xavier Hernandez <xhernan.martinez@gmail.com>
2024-09-01 15:37:30 +0200

    refacto: new OG image and favicon

--- app/app.vue
+++ app/app.vue
@@ -8,14 +8,14 @@
 <script setup lang="ts">
 useHeadSafe({
   link: [
-    { rel: 'icon', type: 'image/png', href: './favicon-32x32.png', sizes: 'any' },
+    { rel: 'icon', type: 'image/png', href: './avatar_midjourney_favicon.png', sizes: 'any' },
   ],
 })
 useSeoMeta({
   ogTitle: 'Xav_ | Frontend Development',
   description: 'Xav_ | Frontend Development',
   ogDescription: 'Xav_ | Frontend Development',
-  ogImage: 'https://www.xavhm.foo/midjourney_home.avif',
+  ogImage: 'https://www.xavhm.foo/Xav_OGImage_optimized.jpg',
   twitterCard: 'summary_large_image',
 })
 </script>
